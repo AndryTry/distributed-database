8410c67a7b9c8dbd743ed66d55e6a7317b517ae5
faisalburhanudin <faisalburhanudin@hotmail.com>
2016-05-04 17:38:57 +0700

    button tambah matakuliah

--- src/templates/matakuliah.php
+++ src/templates/matakuliah.php
@@ -1,7 +1,7 @@
 <?php $this->layout('layout') ?>
 
 <?php $this->start('page') ?>
-    <a class="btn btn-primary" href="#" role="button" data-toggle="modal" data-target="#modalMahasiswa">Tambah mahasiswa</a>
+    <a class="btn btn-primary" href="#" role="button" data-toggle="modal" data-target="#modalMatakuliah">Tambah matakuliah</a>
     <h2>Data matakuliah</h2>
     <table class="table table-striped table-bordered">
         <thead>
@@ -34,17 +34,17 @@
     </table>
 
     <!-- Modal -->
-    <div id=modalMahasiswa class="modal fade" tabindex="-1" role="dialog">
+    <div id=modalMatakuliah class="modal fade" tabindex="-1" role="dialog">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Masukan nim</h4>
+                    <h4 class="modal-title">Masukan kode</h4>
                 </div>
                 <div class="modal-body">
-                    <form class="form" action="/add_mahasiswa.php", method="post">
+                    <form class="form" action="/matakuliah/add", method="get">
                         <div class="form-group">
-                            <input type="text" name="nim" class="form-control" placeholder="nim">
+                            <input type="text" name="kode" class="form-control" placeholder="kode">
                         </div>
                         <button type="submit" class="btn btn-default">simpan</button>
                     </form>
